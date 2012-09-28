ad6f4d99f3284bb977d3046547db5f5afa66916c
Raquel Jimenez Encinar <raquel.jimenez.encinar@cern.ch>
2012-09-28 17:48:49 +0200

    BibFormat: latest posts and comments on blogs and posts
    * Improves the "bfe_blog_posts" bibformat element to:
       * Displays the 3 latest posts on a blog.
       * Offers link "Show all" in order to show all
         the posts on a blog.
    * Improves the "bfe_post_comments" bibformat element to:
       * Displays the 2 latest comments on a post.
       * Offers link "Show all" in order to show all
         the comments on a post.
    
    Conflicts:
    
    	webblog/etc/PostHTML.bft
    	webblog/lib/elements/bfe_post_comments.py

--- webblog/lib/elements/bfe_blog_posts.py
+++ webblog/lib/elements/bfe_blog_posts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## This file is part of CDS Invenio.
-## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+## Copyright (C) 2012 CERN.
 ##
 ## CDS Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -18,37 +18,68 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-BibFormat Element - adds the latest post to its blog
+BibFormat Element - displays the latest posts on a blog
+and it also offers a link to see all the posts of the
+corresponding blog
 """
 
-from invenio.webblog_utils import get_parent_blog, get_posts
+from invenio.webblog_utils import get_posts
 from invenio.search_engine import print_record
 
 
 cfg_messages = {}
-cfg_messages["in_issue"] = {"en": "Latest posts: ",
-                            "fr": "Dernières posts:"}
+cfg_messages["in_issue"] = {"en": "Posts on this blog: ",
+                            "fr": "Posts sur ce blog:"}
 
 
 def format_element(bfo):
     """
+    Displays the latest posts on a blog and it also offers
+    a link to see all the posts of the corresponding blog
     """
 
-    # get variables
     this_recid = bfo.control_field('001')
     current_language = bfo.lang
-    blog_recid = get_parent_blog(this_recid)
-    blog_posts_recids = get_posts(blog_recid, newest_first=True)
+    blog_posts_recids = get_posts(this_recid, newest_first=True)
     out = ""
-
     if blog_posts_recids:
+        # let's print just the 3 latest posts
+        latest_blog_posts_recids = blog_posts_recids[:3]
         out += "<h4>%s</h4>" % cfg_messages["in_issue"][current_language]
 
-        for post_recid in blog_posts_recids:
+        for post_recid in latest_blog_posts_recids:
             out += print_record(post_recid, format='hb')
             out += "<br />"
 
+        all_posts = ""
+        all_blog_posts_recids = blog_posts_recids[3:]
+        for post_recid in all_blog_posts_recids:
+            all_posts += print_record(post_recid, format='hb')
+            all_posts += "<br />"
+
+        out += """
+            <script type="text/javascript">
+            function displayAllPosts(){
+                var all_posts = document.getElementById('all_posts');
+                var see_all_link = document.getElementById('see_all_link');
+                if (all_posts.style.display == 'none'){
+                    all_posts.style.display = '';
+                    see_all_link.innerHTML = "Hide"
+                } else {
+                    all_posts.style.display = 'none';
+                    see_all_link.innerHTML = "Show all"
+                }
+            }
+            </script>
+            """
+
+        out += '<span id="all_posts" style="">' + all_posts + '</span>'
+        out += '<a class="moreinfo" id="see_all_link" \
+                href="javascript:void(0)" onclick="displayAllPosts()""></a>'
+        out += '<script type="text/javascript">displayAllPosts()</script>'
+
     return out
+
 
 def escape_values(bfo):
     """
--- webblog/lib/elements/bfe_post_comments.py
+++ webblog/lib/elements/bfe_post_comments.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## This file is part of CDS Invenio.
-## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+## Copyright (C) 2012 CERN.
 ##
 ## CDS Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,35 +16,68 @@
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
 """
-BibFormat Element - adds the latest comments to its post
+BibFormat Element - displays the latest comments on a post
+and it also offers a link to see all the comments written on
+the corresponding post
 """
 
 from invenio.webblog_utils import get_comments
 from invenio.search_engine import print_record
 
-cfg_messages = {}
-cfg_messages["in_issue"] = {"en": "Here you can see the comments on this post: ",
-                            "fr": "Ici vous pouvez voir les commentaires sur ce post:"}
 
+cfg_messages = {}
+cfg_messages["in_issue"] = {"en": "Comments on this post: ",
+                            "fr": "Commentaires sur ce post:"}
 
 def format_element(bfo):
     """
+    Displays comments on a post
     """
 
     this_recid = bfo.control_field('001')
     current_language = bfo.lang
     post_comments_recids = get_comments(this_recid, newest_first=True)
     out = ""
-
     if post_comments_recids:
+        # let's print just the 3 latest posts
+        latest_post_comments_recids = post_comments_recids[:2]
         out += "<h4>%s</h4>" % cfg_messages["in_issue"][current_language]
 
-        for comment_recid in post_comments_recids:
+        for comment_recid in latest_post_comments_recids:
             out += print_record(comment_recid, format='hb')
             out += "<br />"
 
+        all_comments = ""
+        all_post_comments_recids = post_comments_recids[2:]
+        for comment_recid in all_post_comments_recids:
+            all_comments += print_record(comment_recid, format='hb')
+            all_comments += "<br />"
+
+        out += """
+            <script type="text/javascript">
+            function displayAllComments(){
+                var all_comments = document.getElementById('all_comments');
+                var see_all_link = document.getElementById('see_all_link');
+                if (all_comments.style.display == 'none'){
+                    all_comments.style.display = '';
+                    see_all_link.innerHTML = "Hide"
+                } else {
+                    all_comments.style.display = 'none';
+                    see_all_link.innerHTML = "Show all"
+                }
+            }
+            </script>
+            """
+
+        out += '<span id="all_comments" style="">' + all_comments + '</span>'
+        out += '<a class="moreinfo" id="see_all_link" \
+                href="javascript:void(0)" onclick="displayAllComments()""></a>'
+        out += '<script type="text/javascript">displayAllComments()</script>'
+
     return out
+
 
 def escape_values(bfo):
     """
