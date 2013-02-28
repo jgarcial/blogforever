a87fb4cfc66c9cc2e78fb02ba0758c3c82782218
Raquel Jimenez Encinar <raquel.jimenez.encinar@cern.ch>
2013-02-28 19:08:33 +0100

    WebBlog: add author on posts

---
+++ webblog/lib/elements/bfe_post_author.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+BibFormat Element - displays the author of a post record
+"""
+
+from invenio.config import CFG_SITE_URL
+import cgi
+
+cfg_messages = {}
+cfg_messages["in_issue"] = {"en": "By ",
+                            "fr": ""}
+
+def format_element(bfo):
+    """
+    Displays the author of a post record
+    """
+
+    author = bfo.field('100__a')
+    if author:
+        current_language = bfo.lang
+        out = """%s: <a href = "%s/search?ln=%s&p=%s&f=author">%s</a>""" % \
+                    (cfg_messages["in_issue"][current_language], CFG_SITE_URL, \
+                     current_language, author, author)
+
+    return out
+    
+def escape_values(bfo):
+    """
+    Called by BibFormat in order to check if output of this element
+    should be escaped.
+    """
+    return 0
