8b561a2d8fbe46341afd09c5ce0ad6c4decf4b4a
Raja Sripada <rsripada@cern.ch>
2013-03-06 14:14:42 +0100

    BibCirculation: patron-driven acquisition and more
    * Implementation of patron driven acquisition
      of newly released print books.
    
    * Purchase option to all books for the user.
    
    * Handling groups of similar 'Item's having the
      same 'description' and belonging to the same
      record while issuing loans (multi-volume books etc).
    
    * Automatic and manual recalls of ILLs on the
      lines of normal loans.
    
    * Rectifies error arising when registering a user.
      (closes #1280)
    
    * Other changes involving admin interfaces, code
      reshuffling, refactoring and clean-up.
    
    Tested-by: Tibor Simko <tibor.simko@cern.ch>

--- modules/bibcirculation/lib/bibcirculation_templates.py
+++ modules/bibcirculation/lib/bibcirculation_templates.py
@@ -24,6 +24,7 @@ __revision__ = "$Id$"
 import datetime
 import cgi
 from time import localtime
+
 import invenio.dateutils as dateutils
 from invenio.urlutils import create_html_link
 from invenio.messages import gettext_set_language
@@ -32,6 +33,7 @@ from invenio.config import CFG_SITE_URL, CFG_SITE_LANG, \
      CFG_CERN_SITE, CFG_SITE_SECURE_URL, CFG_SITE_RECORD, \
      CFG_SITE_NAME
 from invenio.messages import gettext_set_language
+
 import invenio.bibcirculation_dblayer as db
 from invenio.bibcirculation_utils import get_book_cover, \
       book_information_from_MARC, \
