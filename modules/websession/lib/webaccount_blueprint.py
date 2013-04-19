eb15c4782d45aaf0aa0b04ac2e20ed3a9819e35b
Raquel Jimenez Encinar <raquel.jimenez.encinar@cern.ch>
2013-04-19 16:54:31 +0200

    REMOVE??? WebSession: port user registration to Flask
    * Ported user registration view to Flask but moving register view from
      websession_webinterface to webaccount_blueprint and creating a new
      WTForm form with validation logic for user registration.
    
    * Fix issue wuth legend icon vertical alignment.
    
    * Fix issue in webuser when dealing with Flask generated legacy
      requests instead of proper legacy requests.
    
    * FIXME Unauthorized access page not ported
    
    Co-authored: Jiri Kuncar <jiri.kuncar@cern.ch>
    
    Conflicts:
    
    	modules/websession/lib/webaccount_blueprint.py

--- modules/websession/lib/webaccount_blueprint.py
+++ modules/websession/lib/webaccount_blueprint.py
@@ -42,6 +42,7 @@ from invenio.access_control_mailcookie import \
     InvenioWebAccessMailCookieError, \
     mail_cookie_check_authorize_action
 from invenio.pluginutils import PluginContainer
+
 from invenio.webaccount_forms import LoginForm, RegisterForm
 from invenio.webuser_flask import login_user, logout_user, current_user
 from invenio.websession_webinterface import wash_login_method
