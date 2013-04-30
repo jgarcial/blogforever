53416f06f8a0d8b7c29048a1c9837f44ee6fd6e5
Alper Cinar <alper@srdc.com.tr>
2013-04-30 16:10:33 +0300

    BibFormat, WebBasket, WebComment, WebMessage, WebStyle: Content Translate - fix
     * TODO: will be reviewed after spider is able to gather language of blogs

--- webblog/lib/elements/bfe_translate.py
+++ webblog/lib/elements/bfe_translate.py
@@ -36,8 +36,8 @@ def format_element(bfo):
     # Insert translate script
     out = ""
     if CFG_TRANSLATE_RECORD_PREVIEW:
-        if current_language != CFG_SITE_LANG:
-            out += get_translate_script('to-be-translated', current_language, True)
+        # if current_language != language_of_blog_post:
+        out += get_translate_script('to-be-translated', current_language, True)
 
     return out
 
