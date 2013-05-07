22c9c1d5f6f238eb1ee7094d535fe8bd7646363f
Raquel Jimenez Encinar <raquel.jimenez.encinar@cern.ch>
2013-05-07 11:44:18 +0200

    BibRank: fix bad indentation
     * In function 'calculate_reading_similarity_list' fixes
       indentation, just go ahead if 'client_host_list != ()'

--- modules/bibrank/lib/bibrank_downloads_similarity.py
+++ modules/bibrank/lib/bibrank_downloads_similarity.py
@@ -92,30 +92,29 @@ def calculate_reading_similarity_list(recid, type="pageviews"):
         client_host_list = client_host_list.replace("L", "")
         client_host_list = client_host_list.replace("[", "")
         client_host_list = client_host_list.replace("]", "")
-
-    # BF: let's find the parent blog of the current blog record (blog, post, comment),
-    # group the results by blog and then display to the user the blogs
-    # that were read by people who also read the parent blog of the current
-    # record (blog, post, comment)
-    if CFG_BLOGFOREVER_SITE:
-        from invenio.webblog_utils import get_parent_blog
-        parent_blog = get_parent_blog(recid)
-        if parent_blog:
-            res = run_sql("SELECT CAST(b.value AS UNSIGNED), COUNT(DISTINCT(client_host)) AS c" \
-                      "  FROM rnkPAGEVIEWS v, bibrec_bib76x bb, bib76x b WHERE client_host IN (" + client_host_list + ")" + \
-                      "   AND v.id_bibrec != %s" \
-                      "   AND v.id_bibrec = bb.id_bibrec" \
-                      "   AND bb.id_bibxxx = b.id" \
-                      "   AND b.tag = '760__w'" \
-                      "   AND b.value != %s" \
-                      " GROUP BY b.value ORDER BY c",
-                      (recid, parent_blog))
-    else:
-        res = run_sql("SELECT id_bibrec,COUNT(DISTINCT(client_host)) AS c" \
-                      "  FROM " + tablename + \
-                      " WHERE client_host IN (" + client_host_list + ")" + \
-                      "   AND id_bibrec != %s" \
-                      " GROUP BY id_bibrec ORDER BY c DESC LIMIT 10",
-                      (recid,))
+        # BF: let's find the parent blog of the current blog record (blog, post, comment),
+        # group the results by blog and then display to the user the blogs
+        # that were read by people who also read the parent blog of the current
+        # record (blog, post, comment)
+        if CFG_BLOGFOREVER_SITE:
+            from invenio.webblog_utils import get_parent_blog
+            parent_blog = get_parent_blog(recid)
+            if parent_blog:
+                res = run_sql("SELECT CAST(b.value AS UNSIGNED), COUNT(DISTINCT(client_host)) AS c" \
+                          "  FROM rnkPAGEVIEWS v, bibrec_bib76x bb, bib76x b WHERE client_host IN (" + client_host_list + ")" + \
+                          "   AND v.id_bibrec != %s" \
+                          "   AND v.id_bibrec = bb.id_bibrec" \
+                          "   AND bb.id_bibxxx = b.id" \
+                          "   AND b.tag = '760__w'" \
+                          "   AND b.value != %s" \
+                          " GROUP BY b.value ORDER BY c",
+                          (recid, parent_blog))
+        else:
+            res = run_sql("SELECT id_bibrec,COUNT(DISTINCT(client_host)) AS c" \
+                          "  FROM " + tablename + \
+                          " WHERE client_host IN (" + client_host_list + ")" + \
+                          "   AND id_bibrec != %s" \
+                          " GROUP BY id_bibrec ORDER BY c DESC LIMIT 10",
+                          (recid,))
 
     return res
